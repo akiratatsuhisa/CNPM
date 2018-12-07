eeaf507bf0254aa2394d8f8e15069e5b272f6699
guiltyshu <36754033+guiltyshu@users.noreply.github.com>
2018-12-07 22:51:30 +0900

    Update CustomersGUI.Designer.cs

--- QuanLyBanHang/QuanLyBanHang/GUI/CustomersGUI.Designer.cs
+++ QuanLyBanHang/QuanLyBanHang/GUI/CustomersGUI.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(821, 474);
             this.Name = "CustomersGUI";
             this.Text = "San Customers";
             this.ResumeLayout(false);
