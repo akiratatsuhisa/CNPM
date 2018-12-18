8f30d8d9026c50f709dcdd76348cfa151e038408
Đăng Khoa Nguyễn <anime.savemylife@gmail.com>
2018-12-18 20:33:47 +0700

    customer

--- QuanLyBanHang/QuanLyBanHang/BUS/CustomersBUS.cs
+++ QuanLyBanHang/QuanLyBanHang/BUS/CustomersBUS.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace QuanLyBanHang.BUS
 {
-    class CustomersBUS
+    public class CustomersBUS
     {
+        private CustomersDAO _customersContext = new CustomersDAO();
+        public List<CustomerDTO> GetList() => _customersContext.GetList().Select(obj => ConvertToCustomerDTO(obj)).ToList();
+        private CustomerDTO ConvertToCustomerDTO(Customer obj) => new CustomerDTO
+        {
+            CustomerID = obj.CustomerID,
+            Name = obj.Name,
+            Gender = obj.Gender ? "Nam" : "Nữ",
+            PhoneNumber = obj.PhoneNumber,
+            Address = obj.Address,
+            Email = obj.Email
+        };
+        private Customer ConvertToCustomer(CustomerDTO obj) => new Customer
+        {
+            CustomerID = obj.CustomerID,
+            Name = obj.Name,
+            Gender = obj.Gender == "Nam" ? true : false,
+            PhoneNumber = obj.PhoneNumber,
+            Address = obj.Address,
+            Email = obj.Email,
+        };
+        public bool AddCustomer(CustomerDTO obj, out string serverMessage) => _customersContext.AddCustomer(ConvertToCustomer(obj), out serverMessage);
+        public bool EditCustomer(CustomerDTO obj, out string serverMessage) => _customersContext.EditCustomer(ConvertToCustomer(obj), out serverMessage);
+        public bool DeleteCustomer(int id, out string serverMessage) => _customersContext.DeleteCustomer(id, out serverMessage);
     }
 }
